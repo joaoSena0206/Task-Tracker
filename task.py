a7c06f92fe990a4b501c3e7b3e3cc34e61ea2d20
João Sena <joaosena0206@gmail.com>
2024-12-25 19:56:39 -0300

    Adiciona classe Task

---
+++ task.py
@@ -0,0 +1,9 @@
+class Task:
+    def __init__(self, id, desc, status, createdAt, updatedAt):
+        self.id = id
+        self.description = desc
+        self.status = status
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+
+        
